efd510db9b77211b341dbda1527c8d7e18c560c6
arsham <arshsm.hajeb@gamil.com>
2025-06-17 16:52:29 +0330

    create maze runner 3d first steps. and start and finish birthdays paradox

---
+++ maze_rinner_3D/maze_runner_3d.py
@@ -0,0 +1,35 @@
+import copy, sys, os
+
+WALL = "#"
+EMPTY = " "
+START = "S"
+EXIT = "E"
+BLOCK = chr(9617)
+NORTH = "NORTH"
+SOUTH = "SOUTH"
+EAST = "EAST"
+WEST = "WEST"
+
+def wallStrToWallDict(wallStr):
+    wallDict = {}
+    height = 0
+    width = 0
+    for y, line in enumerate(wallStr.splitlines()):
+        if y > height:
+            height = y
+        for x, character in enumerate(line):
+            if x > width:
+                width = x
+            wallDict[(x, y)] = character
+    wallDict['height'] = height + 1
+    wallDict['width'] = width + 1
+
+EXIT_DICT = {(0, 0): 'E', (1,0): 'X', (2, 0): 'I', (3, 0): 'T', 'height': 1, 'width': 4}
+
+ALL_OPEN = wallStrToWallDict(r"""
+.................
+___...........___
+...|\......./|...
+...||__...__||...
+
+""")
